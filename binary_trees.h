d26bb1ddd8612d2353a6aa46f8ba6ecb012fcab7
darkalx12 <darkword907@gmail.com>
2024-01-05 04:56:46 -0800

    bee

--- binary_trees.h
+++ binary_trees.h
@@ -89,4 +89,4 @@ int is_perfect_looping(const binary_tree_t *tree,
 int binary_tree_is_perfect(const binary_tree_t *tree);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
 
-#endif /* BINARY_TRE
+#endif /* BINARY_TREES_H */
